d373b8eb51ec6b829d872085c4a5ae72576eb2dc
arireisner <48359633+arireisner@users.noreply.github.com>
2024-02-14 16:36:14 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,263 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-kitchen_360",
+      "name": "Kitchen_360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.4553572290155543,
+        "pitch": 0.15338769443168232,
+        "fov": 1.3635993899637084
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.98614671238812,
+          "pitch": 0.18260195142392455,
+          "rotation": 0,
+          "target": "3-dining_room_1_360"
+        },
+        {
+          "yaw": 2.3616182211440595,
+          "pitch": 0.16299960884537157,
+          "rotation": 0,
+          "target": "2-master_bedroom_1_360"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bathroom_1_360",
+      "name": "Bathroom_1_360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.3077073373184156,
+        "pitch": 0.47912111294391124,
+        "fov": 1.3635993899637084
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3098587194804914,
+          "pitch": 0.16025402697374247,
+          "rotation": 0,
+          "target": "2-master_bedroom_1_360"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-master_bedroom_1_360",
+      "name": "Master_Bedroom_1_360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 3.1367995090167717,
+        "pitch": 0.29896801062741396,
+        "fov": 1.3635993899637084
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.322129822424131,
+          "pitch": 0.13305236353557426,
+          "rotation": 0,
+          "target": "1-bathroom_1_360"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-dining_room_1_360",
+      "name": "Dining_Room_1_360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.6576492042637554,
+        "pitch": 0.2912642736961839,
+        "fov": 1.3635993899637084
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.22082366966755274,
+          "pitch": 0.14146256803185686,
+          "rotation": 0,
+          "target": "4-living_room_1_360"
+        },
+        {
+          "yaw": 1.7841343848027602,
+          "pitch": 0.08789816792559257,
+          "rotation": 0,
+          "target": "0-kitchen_360"
+        },
+        {
+          "yaw": -3.1039695417316917,
+          "pitch": 0.10298559739935342,
+          "rotation": 0,
+          "target": "2-master_bedroom_1_360"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-living_room_1_360",
+      "name": "Living_Room_1_360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 3.121917008706543,
+        "pitch": 0.35534049543543844,
+        "fov": 1.3635993899637084
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1671306211306316,
+          "pitch": 0.08448605781429208,
+          "rotation": 0,
+          "target": "5-front_bedroom_1_360"
+        },
+        {
+          "yaw": 2.921552905181991,
+          "pitch": 0.08866410093114752,
+          "rotation": 0,
+          "target": "3-dining_room_1_360"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-front_bedroom_1_360",
+      "name": "Front_Bedroom_1_360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.031400243366644,
+        "pitch": 0.4239686556903077,
+        "fov": 1.3635993899637084
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7317857738548081,
+          "pitch": 0.14139155071993414,
+          "rotation": 0,
+          "target": "4-living_room_1_360"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "150-35_87th_Ave_Pic_Tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
